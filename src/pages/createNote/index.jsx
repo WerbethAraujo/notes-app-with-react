b13eeefa9cf14bbeeb3185c235711ba3ca58c314
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-06 22:30:49 -0300

    feat: create page create notes

--- src/pages/createNote/index.jsx
+++ src/pages/createNote/index.jsx
@@ -1,5 +1,23 @@
+import { Link } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
+
+import { Header, Form } from './styles';
+
 function CreateNote() {
-  return <div>Create Note</div>;
+  return (
+    <>
+      <Header>
+        <Link to='/' className='btn'>
+          {<IoIosArrowBack />}
+        </Link>
+        <button className='btn lg primary'>Salvar</button>
+      </Header>
+      <Form>
+        <input type='text' autoFocus placeholder='Titulo' />
+        <textarea rows='28' placeholder='Detalhes...'></textarea>
+      </Form>
+    </>
+  );
 }
 
 export default CreateNote;
